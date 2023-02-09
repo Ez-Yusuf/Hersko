9f4b6e6c545491547cb1b44c4e2e84b4e36d0f0a
shipmaster24@outlook.com <shipmaster24@outlook.com>
2023-02-08 22:12:15 -0500

    e

--- Homepage/generategames.js
+++ Homepage/generategames.js
@@ -1,30 +1,32 @@
 //Links
 
 //Games
+
 let Games = {
     "a-dark-room":
     {
-        Name: "A Dark Room"
+        Name: "A Dark Room",
+        Image: "",
+        Description: "awake. head throbbing. vision blurry. come light the fire."
     }
 }
 //Variables
 
 //Code
-function GenerateTest(name)
+function GenerateTest(id)
 {
     var iframe = document.createElement('iframe');
 
     // div tag in which iframe will be added should have id attribute with value myDIV
     document.getElementById("gamesdiv").appendChild(iframe);
-    
     // provide height and width to it
-    iframe.setAttribute("style","height:600px;width:600px;");
+    iframe.setAttribute("style","height:400px;width:400px;");
     iframe.contentWindow.document.open();
-    iframe.src = "Games/Strategy/adarkroom/index.html"
+    iframe.src = "gamespage.html"
     iframe.contentWindow.document.close();
 }
 
 function Generate(id)
 {
-    alert(Games[id]["Name"])
+    GenerateTest(id)
 }
